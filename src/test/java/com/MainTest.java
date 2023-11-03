abae3bd85b928822c8e7cec23a9a2c1fa98632a3
Danny-C-Auditore <1764466133@qq.com>
2023-11-03 14:50:16 +0800

    test sample a-1 b-1

--- src/test/java/com/MainTest.java
+++ src/test/java/com/MainTest.java
@@ -9,11 +9,11 @@ class MainTest {
 	@Test
 	void test() {
 		Main main = new Main();
-//		int result = main.add(1, 1);
-//		assertEquals(2, result);
+		int result = main.add(1, 1);
+		assertEquals(2, result);
 
-		int result2 = main.add(-3, 1);
-		assertEquals(2, result2);
+//		int result2 = main.add(-3, 1);
+//		assertEquals(2, result2);
 	}
 
 }
